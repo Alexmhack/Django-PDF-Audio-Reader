37c29ae3e734f21cf0c76cfd482fbf8712de5583
Alexmhack <alexmohack@gmail.com>
2020-10-16 10:17:47 +0530

    audio pdf model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+AUDIO_VOICES = (
+    (0, 'Male'),
+    (1, 'Female')
+)
+
+
+class PDFAudio(models.Model):
+    text = models.TextField()
+    pdf = models.FileField(upload_to='pdfs')
+    audio_rate = models.IntegerField(blank=True, null=True)
+    volume_level = models.DecimalField(
+        decimal_places=1, max_digits=2,
+        blank=True, null=True)
+    audio_voice = models.CharField(max_length=1, choices=AUDIO_VOICES,
+        default=1)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.pdf.name
