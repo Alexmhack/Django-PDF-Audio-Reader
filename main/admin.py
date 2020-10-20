6123183345ba9386656321ad838a57268b989f06
Alexmhack <alexmohack@gmail.com>
2020-10-21 00:46:55 +0530

    register model to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PDFAudio
+
+
+@admin.register(PDFAudio)
+class PDFAudioAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'get_text', 'pdf', 'audio_rate', 'volume_level',
+        'audio_voice',)
+    list_filter = ('timestamp', 'audio_rate', 'audio_voice',
+        'volume_level')
+
+    def get_text(self, obj):
+        return obj.text[:30]
+
+    get_text.short_description = 'Text'
